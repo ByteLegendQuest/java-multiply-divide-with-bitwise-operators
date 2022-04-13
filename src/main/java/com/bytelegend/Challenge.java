25882b215620c3f302530de1c80858c9d55a988c
1994xiong <xiongmengxm@163.com>
2022-04-13 06:42:16 +0000

    Update at 2022-04-13T06:42:16.678603Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -17,7 +17,7 @@ public class Challenge {
      * (`+`).
      */
     public static int multiplyBy31(int n) {
-         return n << 5; 
+        return mul(n,31);
     }
 
     /**
@@ -32,6 +32,44 @@ public class Challenge {
      * addition sign (`+`).
      */
     public static int divideBy2ThenMinus1(int n) {
-        return ~(n>>1);
+        return sub(n>>1,1); //取反  即-1
+    }
+
+    public static int mul(int a, int b) {
+        int res = 0;
+        while (b != 0) {
+            if ((b & 1) != 0) {
+                res = add(res, a);
+            }
+            a <<= 1;
+            b >>>= 1;
+        }
+        return res;
+    }
+
+    public static int sub(int a, int b) {
+        // 被减数取相反数，使用位运算取反~b+1即b的相反数
+        b = add(~b, 1);
+        // 计算a + (-b)
+        return add(a, b);
+    }
+
+    public static int add(int a, int b) {
+        // 表示两数相加后的和，初始状态是a
+        int sum = a;
+        // 表示进位，初始状态是b，也就是把b当做加和后的进位
+        int carry = b;
+        // 循环条件：只要进位不为0，则继续加
+        while (carry != 0) {
+            // 计算两数对应二进制位加和
+            sum = a ^ b;
+            // 记录每一位的进位信息，<<将每一位的进位信息提高一位，加入到下次对位计算中
+            carry = (a & b) << 1;
+            // a 重新赋值加和结果
+            a = sum;
+            // 记录进位
+            b = carry;
+        }
+        return sum;
     }
 }
