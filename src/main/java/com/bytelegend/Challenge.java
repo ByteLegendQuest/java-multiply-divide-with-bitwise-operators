d65050780bf452afba80029ba5ed2ea613c9f18f
cdwc233 <2241830325@outlook.com>
2023-03-05 14:43:19 +0000

    Update at 2023-03-05T14:43:19.059406Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -17,7 +17,16 @@ public class Challenge {
      * (`+`).
      */
     public static int multiplyBy31(int n) {
-        return 0;
+        int sum = 0;
+        int a = 31;
+        while (n != 0) {
+            if ((n & 1) != 0) {
+                sum += a;
+            }
+            a <<= 1;
+            n >>>= 1;
+        }
+        return sum;
     }
 
     /**
@@ -31,7 +40,45 @@ public class Challenge {
      * <p>Note that you are not allowed to use multiplication sign (`*`), division sign (`/`), and
      * addition sign (`+`).
      */
+    public static int bitAdd(int a, int b) {
+        int sum = 0;
+        while (b != 0) {
+            sum = a ^ b;
+            b = (a & b) << 1;
+            a = sum;
+        }
+        return sum;
+    }
+
+    public static int negNum(int n) {
+        //转化为相反数
+        return bitAdd(~n, 1);
+    }
+
+    public static int minus(int a, int b) {
+        //实现两个数相减
+        return bitAdd(a, negNum(b));
+    }
+
+    public static boolean isNeg(int n) {
+        //判断是否为负数
+        return n < 0;
+    }
+
+    public static int bitDiv(int a, int b) {
+        int x = isNeg(a) ? negNum(a) : a;
+        int y = isNeg(b) ? negNum(b) : b;
+        int res = 0;
+        for (int i = 30; i >= 0; i = bitAdd(i, negNum(1))) {
+            if ((x >> i) >= y) {
+                res |= (1 << i);
+                x = bitAdd(x, negNum(y << i));
+            }
+        }
+        return isNeg(a) != isNeg(b) ? negNum(res) : res;
+    }
+
     public static int divideBy2ThenMinus1(int n) {
-        return 0;
+        return bitAdd(bitDiv(n, 2), ~1 + 1);
     }
 }
