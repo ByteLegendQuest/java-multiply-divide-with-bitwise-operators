cd47eae2aa13d1fb0e259a669801a612b4093cd9
karolk75 <karolk410410@gmail.com>
2024-01-04 12:53:05 +0000

    Update at 2024-01-04T12:53:05.373215Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -17,7 +17,7 @@ public class Challenge {
      * (`+`).
      */
     public static int multiplyBy31(int n) {
-        return 0;
+        return (n << 5) - n;
     }
 
     /**
@@ -32,6 +32,8 @@ public class Challenge {
      * addition sign (`+`).
      */
     public static int divideBy2ThenMinus1(int n) {
-        return 0;
+        // 0000 0101 - 5
+        // 0000 0010 - 2
+        return (n >> 1) - 1;
     }
 }
